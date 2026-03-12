ad7571c64a178a94cce95217536ee7fb9b5c0b37
KTH <taehyeonentus@gmail.com>
2026-03-13 06:25:56 +0900

    feat(front) : add proxy

--- frontend/lib/api/apiClient.ts
+++ frontend/lib/api/apiClient.ts
@@ -2,7 +2,7 @@ import axios, { AxiosRequestConfig } from "axios";
 
 // 1. 기본 설정 (Base Instance)
 const axiosInstance = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_BACKEND_URL || "https://api.chessopeningstat.com",
+    baseURL: "/api",
     headers: {
         "Content-Type": "application/json",
     },
--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -3,6 +3,15 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin('./i18n.ts');
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async rewrites() {
+        return [
+            {
+                source: '/api/:path*',
+                destination: `${process.env.NEXT_PUBLIC_BACKEND_URL || 'https://api.chessopeningstat.com'}/:path*`,
+            },
+        ];
+    },
+};
 
 export default withNextIntl(nextConfig);
