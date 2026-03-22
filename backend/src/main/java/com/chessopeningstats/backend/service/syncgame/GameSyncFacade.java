9af46516023c990c8816e4e4b24736e23f8ef70d
KTH <taehyeonentus@gmail.com>
2026-03-23 00:14:41 +0900

    chore(back) : update comment on main business logic

--- backend/src/main/java/com/chessopeningstats/backend/service/syncgame/GameSyncFacade.java
+++ backend/src/main/java/com/chessopeningstats/backend/service/syncgame/GameSyncFacade.java
@@ -2,13 +2,11 @@ package com.chessopeningstats.backend.service.syncgame;
 
 import com.chessopeningstats.backend.domain.Player;
 import com.chessopeningstats.backend.infra.client.playergames.dto.RawGame;
-import com.chessopeningstats.backend.service.syncgame.dto.AnalyzedGame;
 import com.chessopeningstats.backend.service.syncgame.registry.GameFetchServiceRegistry;
 import com.chessopeningstats.backend.service.syncgame.registry.GameNormalizeServiceRegistry;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
 
@@ -27,16 +25,22 @@ public class GameSyncFacade {
         GameFetchService<RawGame> gameFetchService = gameFetchServiceRegistry.getService(player.platform());
         GameNormalizeService<RawGame> gameNormalizeService = gameNormalizeServiceRegistry.getService(player.platform());
 
+        //네트워크 바운드
         return gameFetchService.fetch(player)
+
+                //CPU 바운드 (병렬 전환)
                 .parallel()
                 .runOn(Schedulers.parallel())
                 .map(gameNormalizeService::normalize)
                 .filter(gameSanitizeService::sanitize)
                 .map(gameAnalyzeService::analyze)
+
+                //기록 합산을 위해 직렬화
                 .sequential()
-                .groupBy(AnalyzedGame::player)
-                .flatMap(Flux::collectList)
+                .collectList()
                 .map(dashboardConvertService::convertDashboard)
+
+                //I/O 바운드 (현재는 캐시만 DB X)
                 .publishOn(Schedulers.boundedElastic())
                 .doOnNext(dashboardCacheService::cacheDashboard)
                 .then();
